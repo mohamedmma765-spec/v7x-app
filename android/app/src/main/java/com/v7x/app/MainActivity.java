856a84a3c714c80d3e6041b8b4ecd381a1afc70a
root <root@srv1018841.hstgr.cloud>
2026-03-13 20:31:14 +0000

    Critical: Manual URL override in Java and Strings

--- android/app/src/main/java/com/v7x/app/MainActivity.java
+++ android/app/src/main/java/com/v7x/app/MainActivity.java
@@ -1,5 +1,12 @@
 package com.v7x.app;
-
+import android.os.Bundle; // ضيف السطر ده لو مش موجود
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // إجبار التطبيق على فتح الرابط بتاعك فوراً
+        this.bridge.getWebView().loadUrl("https://v7x.fun/user/login");
+    }
+}
